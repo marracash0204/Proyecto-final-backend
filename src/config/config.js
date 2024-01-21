a04ed7df5b2ad25ad419e59032a7614ca5f0fd91
Camilo A. Marin Lopez <marracash0204@gmail.com>
2024-01-21 15:57:05 -0300

    prueba vercel

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   nodeEnv: process.env.NODE_ENV,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGODB_URL,
   sessionSecret: process.env.SESSION_SECRET,
   passportGitHubClientId: process.env.PASSPORT_GITHUB_CLIENT_ID,
   passportGitHubClientSecret: process.env.PASSPORT_GITHUB_CLIENT_SECRET,
